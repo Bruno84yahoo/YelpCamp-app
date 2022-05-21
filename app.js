fb9ccb95e23cb5136f7706f69c01b416347c7755
Bruno84yahoo <brunosimoni84@yahoo.com>
2022-05-21 12:02:13 +0900

    Build layout and files set up Install ejs-mate and Bootstrap 5(no jquery) Define boilerplate with code  between contents

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 // shortcut
 const mongoose = require('mongoose');
+const ejsMate = require("ejs-mate");
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -16,6 +17,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
