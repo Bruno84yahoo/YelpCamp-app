6f16e83d4961bb9027eee7c06010d8dbe683dad0
Bruno84yahoo <brunosimoni84@yahoo.com>
2022-06-05 14:25:12 +0900

    Authentication Install Passport and create user model Configure Passport Set up Register Form Register Route Logic Create  2Login Routes Add isLoggedIn Middleware and fix  /:id route Add Logout Define currentUser and  display on nav barFix Register Route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const validateCampground = (req, res, next) => {
@@ -18,10 +19,12 @@ router.get('/', catchAsync(async (req, res) => {
 const campgrounds = await Campground.find({});
 res.render('campgrounds/index', { campgrounds })
 }));
-router.get('/new', (req, res) => {
-res.render('campgrounds/new');
+
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('campgrounds/new');
 })
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
 // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 const campground = new Campground(req.body.campground);
 await campground.save();
@@ -30,6 +33,15 @@ res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.get('/:id', catchAsync(async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}));
+
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 const campground = await Campground.findById(req.params.id).populate('reviews');
 if (!campground) {
     req.flash('error', 'Cannot find that campground!');
